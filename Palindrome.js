aa7ba734af3c00aea68833a0465f1808ac66b5ab
Arunava Chakraborty <arunavaskar@outlook.com>
2019-11-10 17:19:30 +0530

    done

---
+++ Palindrome.js
@@ -0,0 +1,12 @@
+function palindromeCheck(palindrome){
+    var newstr = '';
+    var length = palindrome.length - 1;
+    var count = length;
+    while (count >= 0){
+        newstr += palindrome[count].toLowerCase();
+        count -= 1;
+    }
+    return newstr == palindrome.toLowerCase();
+}
+
+console.log(palindromeCheck("Radar"))
---
+++ Palindrome.py
@@ -0,0 +1,10 @@
+def palindromeCheck(palindrome):
+  newstr = ''
+  length = len(palindrome) - 1
+  count = length
+  while count >= 0:
+    newstr += palindrome[count].lower()
+    count -= 1 
+  return newstr == palindrome.lower()
+
+print(palindromeCheck("Radar"))
