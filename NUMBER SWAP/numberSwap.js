12945711c4b16d90475c216601eb2f8ea0038de2
Arunava Chakraborty <arunavaskar@outlook.com>
2019-11-14 22:04:24 +0530

    done

---
+++ NUMBER SWAP/numberSwap.js
@@ -0,0 +1,10 @@
+var a = 12;
+var b = 20;
+console.log(a, 'a')
+console.log(b, 'b')
+console.log("swapping....")
+var a = a + b;
+var b = a - b;
+var a = a - b;
+console.log(a, 'a')
+console.log(b, 'b')
---
+++ NUMBER SWAP/numberSwap.py
@@ -0,0 +1,10 @@
+a = 12
+b = 20
+print(a,'a')
+print(b, 'b')
+print("swapping....")
+a = a + b
+b = a - b
+a = a - b
+print(a, 'a')
+print(b, 'b')
